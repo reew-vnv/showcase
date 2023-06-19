8d48433228a5b3c1ff3c108c8e9913d4bae747f8
aivanov <aivanov@t-systems.com>
2023-06-19 15:05:27 +0200

    prdo

--- src/pages/about/ui/about.lazy.tsx
+++ src/pages/about/ui/about.lazy.tsx
@@ -1,9 +1,3 @@
 import { lazy } from 'react';
 
-// export const AboutLazy = lazy(() => import('./about'));
-
-export const AboutLazy = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./about')), 1500);
-}));
+export const AboutLazy = lazy(() => import('./about'));
